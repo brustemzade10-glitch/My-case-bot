9d32f59bd4ec5c7a0b3f31af5d5ab8f12b3a38c9
brustemzade10-glitch <brustemzade10@gmail.com>
2026-06-04 16:57:47 +0400

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '8624933164:AAE3x266mB4UTp-T6LX6QghY4QBUM1AVWzo';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Kasanı açmaq üçün aşağıdakı düyməni sıx!", {
+        reply_markup: {
+            inline_keyboard: [[
+                { text: "Kasanı Aç", web_app: { url: "https://google.com" } }
+            ]]
+        }
+    });
+});
